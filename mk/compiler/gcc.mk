5bff5d151d3a7a00607f6b68f951d18537553b50
Jonathan Perkin <jonathan@perkin.org.uk>
2024-04-22 11:37:52 +0100

    mk: Allow _GCC_REQD to be precomputed.

--- mk/compiler/gcc.mk
+++ mk/compiler/gcc.mk
@@ -449,6 +449,7 @@ _IS_BUILTIN_GCC=	NO
 # Distill the GCC_REQD list into a single _GCC_REQD value that is the
 # highest version of GCC required.
 #
+.if !defined(_GCC_REQD)
 _GCC_STRICTEST_REQD?=	none
 .for _version_ in ${GCC_REQD}
 .  for _pkg_ in gcc-${_version_}
@@ -471,6 +472,7 @@ _GCC_STRICTEST_REQD=	${_version_}
 .  endfor
 .endfor
 _GCC_REQD=	${_GCC_STRICTEST_REQD}
+.endif
 
 # Determine which GCC version is required by examining _GCC_REQD.
 _NEED_GCC6?=	no
