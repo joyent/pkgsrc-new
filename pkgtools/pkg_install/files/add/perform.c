db430fbb95d88987e6c324c32015a1d6be2b49b2
Jonathan Perkin <jperkin@smartos.org>
2024-11-28 15:46:30 +0000

    pkg_install: Disable check_requires().
    Still has a number of problems, and the very last thing we want is to be
    stuck in a situation that we might not be able to recover from.  The bug
    this code was implemented to solve does not really apply to us, and our
    users are trained to use pkgin not pkg_add.

--- pkgtools/pkg_install/files/add/perform.c
+++ pkgtools/pkg_install/files/add/perform.c
@@ -1137,6 +1137,7 @@ check_requires(struct pkg_task *pkg)
 	const char *data, *eol, *next_line;
 	int ret = 0;
 
+#if 0
 	data = pkg->meta_data.meta_build_info;
 
 	for (; data != NULL && *data != '\0'; data = next_line) {
@@ -1176,6 +1177,7 @@ check_requires(struct pkg_task *pkg)
 			free(libpath);
 		}
 	}
+#endif
 
 	return ret;
 }
